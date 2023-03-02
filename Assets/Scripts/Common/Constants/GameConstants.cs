e024dae61ea46563bc893400ba0188e2439567c0
Nimiod <Nimiod02@gmail.com>
2023-03-02 23:23:10 +0700

    Feat(Common) - игровые константы

---
+++ Assets/Scripts/Common/Constants/GameConstants.cs
@@ -0,0 +1,7 @@
+public static class GameConstants
+{
+    public static float HEALTH_PER_ENDURANCE = 5f;
+    public static float STAMINA_PER_AGILITY = 5f;
+    public static float STAMINA_PER_PERCEPTION = 5f;
+    public static float MANA_PER_INTELLIGENCE = 5f;
+}
